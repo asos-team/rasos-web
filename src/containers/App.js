ca5fa4d67384458fd8b801564befefb3fef6a8bc
IdoAgam <idow09@gmail.com>
2018-05-28 14:58:43 +0300

    Introduced Dropzone

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,21 +1,74 @@
 import React, { Component } from 'react';
 import logo from '../logo.svg';
 import './App.css';
+import Dropzone from 'react-dropzone'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            accept: '',
+            files: [],
+            dropzoneActive: false
+        }
+    }
+
+    onDragEnter() {
+        this.setState({
+            dropzoneActive: true
+        });
+    }
+
+    onDragLeave() {
+        this.setState({
+            dropzoneActive: false
+        });
+    }
+
+    onDrop(files) {
+        this.setState({
+            files,
+            dropzoneActive: false
+        });
+    }
+
+    render() {
+        const { files, dropzoneActive } = this.state;
+        const overlayStyle = {
+            position: 'absolute',
+            top: 0,
+            right: 0,
+            bottom: 0,
+            left: 0,
+            padding: '2.5em 0',
+            background: 'rgba(0,0,0,0.5)',
+            textAlign: 'center',
+            color: '#fff'
+        };
+        return (
+            <Dropzone
+                style={{position: "relative"}}
+                multiple={false}
+                onDrop={this.onDrop.bind(this)}
+                onDragEnter={this.onDragEnter.bind(this)}
+                onDragLeave={this.onDragLeave.bind(this)}
+            >
+                { dropzoneActive && <div style={overlayStyle}>Drop files...</div> }
+                <div>
+                    <h1>Rasos Training Uploader</h1>
+                    <h2>Player:</h2>
+                    <ul>
+                        {
+                            files.map(f => <li>{f.name} - {f.size} bytes</li>)
+                        }
+                    </ul>
+
+                </div>
+            </Dropzone>
+        );
+    }
 }
+
+<App />
 
 export default App;
